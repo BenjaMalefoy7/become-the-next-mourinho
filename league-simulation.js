d363c436d5c7336f3aa1bff4a0c929ce76f59dc8
BenjaMalefoy7 <skezark@gmail.com>
2026-06-14 20:48:07 +0200

    Remove unused league simulation bridge

--- league-simulation.js
+++
@@ -1,9 +0,0 @@
-const BTM_LEAGUE_SIM_ENTRYPOINT_VERSION="0.22";
-(function(){
-if(document.querySelector('script[src*="matchday-v090.js"]'))return;
-const s=document.createElement('script');
-s.src='matchday-v090.js?v=022';
-s.async=false;
-s.onload=()=>{if(typeof refreshUI==='function')refreshUI();};
-document.body.appendChild(s);
-})();
